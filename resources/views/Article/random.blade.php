07752d2de9ae87ea2ee8d61ed6e17eca94def8f7
ankitjain28may <ankitjain28may77@gmail.com>
2017-06-10 17:51:58 +0530

    checking

--- resources/views/Article/random.blade.php
+++ resources/views/Article/random.blade.php
@@ -1,14 +1,13 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta property="og:url"                content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
-        <meta property="og:type"               content="article" />
-        <meta property="og:title"              content="When Great Minds Don’t Think Alike" />
-        <meta property="og:description"        content="How much does culture influence creative thinking?" />
-        <meta property="og:image"              content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
+
+        <meta property="og:url" content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="When Great Minds Don’t Think Alike" />
+        <meta property="og:description" content="How much does culture influence creative thinking?" />
+        <meta property="og:image" content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
+
         <title>Ankit Jain</title>
 
         <!-- Fonts -->
