6a688598045cd30b991b191745c7754dd9f877fe
ankitjain28may <ankitjain28may77@gmail.com>
2017-06-10 17:35:22 +0530

    checking

--- resources/views/Article/random.blade.php
+++ resources/views/Article/random.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta property="og:type" content="website"/>
-        <meta property="og:title" content="{{ $data['name'] }}"/>
+        <meta property="og:title" content="Ankit"/>
         <meta property="og:description" content="{{ $data['description'] }}" />
         <meta property="og:image" content="{{ $data['image_url'] }}" />
         <meta property="og:url" content="https://random-articles.herokuapp.com/" />
